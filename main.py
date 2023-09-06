0023f2d5fdc394ba169cd757c26c72a0fa4cd4e0
Praddyumn Yadav <praddyumny@gmail.com>
2023-09-06 23:18:23 +0530

    Create trigger_notification() functions.

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import soundfile as sf
 from plyer import notification
 
 
+def trigger_notification(number, title, message, ringtone):
+    # Trigger the Notification
+    notification.notify(
+        title=(title + " - " + str(number)),
+        message=message,
+        app_icon="logo.jpeg",
+        timeout=10,
+    )
+
+    # Initialize Pygame
+    pygame.init()
+
+    # Load and Play the Ringtone
+    pygame.mixer.music.load(ringtone)
+    pygame.mixer.music.play()
+
+    # Start Timer for 15 Seconds
+    time.sleep(get_audio_length(ringtone))
+
+    # Stop the Ringtone
+    pygame.mixer.music.stop()
+
+    # Quit Pygame
+    pygame.quit()
+
+
 # Create a Function for Checking Length of Audio
 def get_audio_length(filename):
     with sf.SoundFile(filename) as f:
