2580ddc8ebc888010ea3f39467e31b2213906f58
Praddyumn Yadav <praddyumny@gmail.com>
2023-09-06 23:17:40 +0530

    Create get_audio_length() function.

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pygame
 import soundfile as sf
 from plyer import notification
 
+
+# Create a Function for Checking Length of Audio
+def get_audio_length(filename):
+    with sf.SoundFile(filename) as f:
+        return len(f) / f.samplerate
